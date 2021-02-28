0082710dd41757777b0c76751198fd77c33d9b57
Carlitos <cavelo@bemen3.cat>
2021-02-28 16:06:20 +0100

    f

---
+++ src/m05_uf2_p4/M05_UF2_P4.java
@@ -0,0 +1,16 @@
+package m05_uf2_p4;
+
+/**
+ *
+ * @author carlitos
+ */
+public class M05_UF2_P4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
